e60a5ef1815baf600252c1846276a1f40ed34d8a
Vij14 <113373765+Vij14@users.noreply.github.com>
2023-12-01 08:25:47 +0530

    Add files via upload

---
+++ histogram.R
@@ -0,0 +1,4 @@
+hist(v,main,xlab,ylab,col,border)  
+v <-c(12,24,16,38,21,13,55,17,39,10,60) 
+main<-c(1,2,3,4,5,6,7,8,9,10,11)
+hist(v,main,xlab = "Weight",ylab="Frequency",col= "green",border = "red")  
